c0c94aa308c56dca60dbab1c17eb58daba165223
Leakey Maina <leakeymaina@gmail.com>
2022-10-25 13:30:08 +0300

    Added mpesaTest.py

--- routes/Payments/mpesa.py
+++ routes/Payments/mpesa.py
@@ -1,12 +1,11 @@
 
-from cmath import inf
 import requests
 import asyncio
 from enum import Enum
 
 
 class MpesaEndPoints(Enum):
-    base_url = "https://sandbox.safaricom.co.ke/"
+    base_url = "https://sandbox.safaricom.co.ke"
     get_access_token = "/oauth/v1/generate?grant_type=client_credentials"
     b2b_payment_request = "/mpesa/b2b/v1/paymentrequest"
     reverse_transaction = "/mpesa/reversal/v1/request"
@@ -23,40 +22,13 @@ class MpesaApiWrapper:
     def __init__(self, base_url=MpesaEndPoints.base_url.value, headers={
             'Authorization': 'Basic amJDTnBmakZ5UHdqVWRmWHpCNThGQW9CN3VMUkdDQnM6S2FIRUhlaTM3WUtXS2RDbA=='}):
         self.__base_url = base_url
-        self.__auth_url = self.__base_url + \
-            MpesaEndPoints.get_access_token.value
         self.__request_headers = headers
 
     async def get_access_token(self):
         print("Called MPESA API...get_access_token")
+        auth_url = self.__base_url + MpesaEndPoints.get_access_token.value
         response = requests.request(
-            "GET", self.__auth_url, headers=self.__request_headers)
+            "GET", auth_url, headers=self.__request_headers)
         await asyncio.sleep(1)
 
         return response.json()
-
-
-async def other_task():
-    print("Running other tasks...waiting on access token")
-    for i in range(10):
-        print(f"Running other tasks...{i}")
-        await asyncio.sleep(0.1)
-
-
-async def main():
-    mpesa_wrapper = MpesaApiWrapper()
-
-    task1 = asyncio.create_task(
-        mpesa_wrapper.get_access_token())
-
-    task2 = asyncio.create_task(other_task())
-
-    response = await task1
-
-    print(f'RECEIVED RESPONSE {response}')
-    print(f'ACCESS TOKEN = {response["access_token"]}')
-    print(f'EXPIRES IN = {response["expires_in"]}')
-
-    await task2
-
-asyncio.run(main())
