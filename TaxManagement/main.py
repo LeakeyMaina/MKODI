45d34ba39c12649ca99863075428736a86f384f9
Leakey Maina <leakeymaina@gmail.com>
2022-10-26 13:08:17 +0300

    restructured the project folders

---
+++ TaxManagement/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from Routes import landlords, properties, tenants
+
+app = FastAPI()
+
+app.include_router(landlords.router)
+app.include_router(properties.router)
+app.include_router(tenants.router)
+
+
+@app.get("/")
+def root():
+    return "Hello, Welcome to MKODI Tax management API"
