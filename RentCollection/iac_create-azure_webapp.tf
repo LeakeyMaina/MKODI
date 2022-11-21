e85a6438055809e2ab8f8ab1456ad7c38ef34a8f
Leakey Maina <leakeymaina@gmail.com>
2022-11-21 19:50:33 +0300

    modified the IAC terraform file

--- RentCollection/iac_create-azure_webapp.tf
+++ RentCollection/iac_create-azure_webapp.tf
@@ -3,6 +3,8 @@
 # An Azure KeyVault instance can store application secrets such as connection strings.
 # Database server—Azure Database for Postgresql 
 # Service Bus to allow asynchronous communication between the services.
+
+
 terraform {
   required_providers {
     azurerm = {
@@ -12,6 +14,14 @@ terraform {
   }
 }
 
+# Terraform needs a service principal to create and destroy resources on Azure. 
+# the newly created app registration needs the Contributor access to read from Azure and the write access 
+# to create new resources. To keep things simple, give the Contributor role to the registered app at 
+# the subscription level running the following command:
+
+# az ad sp create-for-rbac --name sshsapp --role contributor --scope "/subscriptions/<subscriptionId>" --sdk-auth where:
+# subscriptionId is your Azure subscription
+
 provider "azurerm" {
   subscription_id = "17a163e1-cd14-4a6a-80c4-05945f55fa55"
   client_id       = "bef8bc2e-ee98-4fee-95d1-736c7a549f7b"
@@ -20,30 +30,32 @@ provider "azurerm" {
   features {}
 }
 
-
 locals {
-  resource_group="app-grp"
-  location="North Europe"
+  resource_group=azurerm_resource_group.mkodi_resource_grp.name
+  location=azurerm_resource_group.mkodi_resource_grp.location
+  service_plan_id =azurerm_service_plan.mkodi_service_plan.id
+}
+
+resource "azurerm_resource_group" "mkodi_resource_grp"{
+  name="mkodi_resource_grp"
+  location="eastus"
 }
 
 
-resource "azurerm_resource_group" "app_grp"{
-  name=local.resource_group
-  location=local.location
-}
-resource "azurerm_service_plan" "mkodi-service-plan" {
-  name                = "mkodi-service-plan"
-  location            = azurerm_resource_group.app_grp.location
-  resource_group_name = azurerm_resource_group.app_grp.name
+resource "azurerm_service_plan" "mkodi_service_plan" {
+  name                = "mkodi_service_plan"
+  resource_group_name = local.resource_group
+  location            = local.location
   os_type             = "Windows"
   sku_name            = "B1"
 }
 
+
 resource "azurerm_windows_web_app" "mkodi" {
   name                = "mkodi"
-  resource_group_name = azurerm_resource_group.app_grp.name
-  location            = azurerm_resource_group.app_grp.location
-  service_plan_id     = azurerm_service_plan.mkodi-service-plan.id
+  resource_group_name = local.resource_group
+  location            = local.location
+  service_plan_id     = local.service_plan_id
 
   site_config {}
 }
